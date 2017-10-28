d570952d5f1db603d1392a8b75b6b9ea6a6e38a2
aitorsanz <aitor448@gmail.com>
2017-10-28 18:13:45 +0200

    SQL´s

---
+++ sql/create table jugadores y partidos.sql
@@ -0,0 +1,25 @@
+CREATE TABLE jugadores (
+  id int(11) AUTO_INCREMENT NOT NULL,
+  nombre VARCHAR(50) DEFAULT NULL ,
+  apellidos VARCHAR(50) DEFAULT NULL,
+  correo VARCHAR(50) DEFAULT NULL,
+  nivel FLOAT DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE partidos (
+  id int(11) AUTO_INCREMENT NOT NULL,
+  fkIdJugador1 int(11) DEFAULT NULL,
+  fkIdJugador2 int(11) DEFAULT NULL,
+  fkIdJugador3 int(11) DEFAULT NULL,
+  fkIdJugador4 int(11) DEFAULT NULL,
+  lugar VARCHAR(100) DEFAULT NULL,
+  hora TIME DEFAULT NULL,
+  fecha DATE DEFAULT NULL,
+  nivel FLOAT DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+ALTER TABLE partidos ADD CONSTRAINT `partidos_jugador1` FOREIGN KEY  (`fkIdJugador1`) REFERENCES jugadores(`id`) on UPDATE CASCADE ;
+ALTER TABLE partidos ADD CONSTRAINT `partidos_jugador2` FOREIGN KEY  (`fkIdJugador2`) REFERENCES jugadores(`id`) on UPDATE CASCADE ;
+ALTER TABLE partidos ADD CONSTRAINT `partidos_jugador3` FOREIGN KEY  (`fkIdJugador3`) REFERENCES jugadores(`id`) on UPDATE CASCADE ;
+ALTER TABLE partidos ADD CONSTRAINT `partidos_jugador4` FOREIGN KEY  (`fkIdJugador4`) REFERENCES jugadores(`id`) on UPDATE CASCADE ;
---
+++ sql/insert para pruebas.sql
@@ -0,0 +1,33 @@
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (1,'Aitor','Sanz Vidal','aitor_sanz@hotmail.com',5);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (2,'Gage','Farley','ligula.Aenean.euismod@nunc.com',7);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (3,'Wylie','Good','ligula@orciluctus.edu',2);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (4,'Paloma','Richard','Proin.nisl.sem@lacus.com',6);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (5,'Evan','Walters','amet@cursusluctusipsum.com',1);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (6,'Kennedy','Rasmussen','felis.ullamcorper.viverra@magnaSedeu.com',9);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (7,'Venus','Noble','amet.metus.Aliquam@etlaciniavitae.com',5);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (8,'Ruby','Ross','nec.mauris.blandit@Sedcongueelit.org',3);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (9,'Cailin','Burton','nec.imperdiet.nec@consequatauctor.co.uk',1);
+INSERT INTO jugadores (id,nombre,apellidos,correo,nivel) VALUES (10,'Bradley','Mccarty','Duis.cursus@nonummyipsum.co.uk',5);
+
+INSERT INTO partidos (id,fkIdJugador1,fkIdJugador2,fkIdJugador3,fkIdJugador4,lugar,fecha,hora,nivel) VALUES
+ (2,8,9,1,3,"701 Pede. St.","08:08:08",'2017-01-01',7),
+ (3,6,5,6,8,"6106 Tincidunt Street",'08:08:08','2017-01-01',6),
+ (4,4,6,10,6,"P.O. Box 889, 4814 Quam. St.",'08:08:08','2017-01-01',8),
+ (5,1,2,1,1,"7135 Nibh Ave",'08:08:08','2017-01-01',8),
+ (6,3,10,3,4,"7225 Donec Ave",'08:08:08','2017-01-01',6),
+ (7,4,7,9,3,"P.O. Box 222, 3224 Sed Av.",'08:08:08','2017-01-01',7),
+ (8,4,10,4,10,"337-8946 Vulputate Avenue",'08:08:08','2017-01-01',1),
+ (9,3,8,5,7,"Ap #177-2094 Accumsan St.",'08:08:08','2017-01-01',6),
+ (10,1,3,10,7,"P.O. Box 672, 1016 Dolor. Av.",'08:08:08','2017-01-01',7),
+ (11,10,3,1,1,"P.O. Box 983, 2475 Pretium Ave",'08:08:08','2017-01-01',2);
+INSERT INTO partidos (id,fkIdJugador1,fkIdJugador2,fkIdJugador3,fkIdJugador4,lugar,fecha,hora,nivel) VALUES 
+(12,1,9,2,7,"523-8119 Dolor. Rd.",'08:08:08','2017-01-01',4),
+(13,1,6,9,9,"Ap #359-7457 Fusce Av.",'08:08:08','2017-01-01',9),
+(14,2,4,7,7,"P.O. Box 750, 2490 Vulputate Rd.",'08:08:08','2017-01-01',8),
+(15,7,2,5,9,"662-5415 Iaculis Avenue",'08:08:08','2017-01-01',3),
+(16,3,9,9,10,"Ap #733-6904 Odio. St.",'08:08:08','2017-01-01',10),
+(17,3,7,3,4,"3273 Id Street",'08:08:08','2017-01-01',8),
+(18,9,10,7,3,"P.O. Box 662, 7354 Netus Av.",'08:08:08','2017-01-01',7),
+(19,5,3,3,8,"974-3679 Nec, Street",'08:08:08','2017-01-01',1),
+(20,5,9,4,8,"P.O. Box 161, 2303 Cum Rd.",'08:08:08','2017-01-01',10),
+(21,8,6,1,9,"P.O. Box 107, 3843 Donec Av.",'08:08:08','2017-01-01',3);
